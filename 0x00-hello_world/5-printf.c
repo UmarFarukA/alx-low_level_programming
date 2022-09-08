c1768156915bec62c160a53914e38a9ae946395c
ufaz <talk2ufaz@gmail.com>
2022-09-08 14:03:43 -0700

    Added:5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+/*
+ *File: 5-printf
+ *Auth: Umar Faruk A<talk2ufaz@gmail.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Print with proper grammar, but the outcome is a piece of art
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+
+	return (0);
+}
