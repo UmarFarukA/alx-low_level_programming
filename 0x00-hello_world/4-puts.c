acadae276dce294fcc56e39be05e1e6c5f86d334
ufaz <talk2ufaz@gmail.com>
2022-09-08 13:33:17 -0700

    Added:4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,21 @@
+/**
+ * File: 4-puts.c
+ * Author: Umar Faruk A 
+ *
+ */
+
+
+
 #include <stdio.h>
 
-
+/**
+ *
+ * main - Prints "Programming is like building a multilingual puzzle
+ *
+ *
+ * Return: Always 0.
+ *
+ * */
 int main(void)
 {
 
