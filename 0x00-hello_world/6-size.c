bd0ed0166f3d7ab7481bb593b450f9419f941913
ufaz <talk2ufaz@gmail.com>
2022-09-08 14:32:25 -0700

    Added: 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+/**
+ * File: Prints the size of various types on the computer 
+ * Auth: Umar Faruk A<talk2ufaz@gmail.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main: Print the size of  various types on the commputer
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	printf("Size of char in: %u bytes", sizeof(char));
+	printf("size of int: %u in bytes", sizeof(int));
+	printf("Size of long int: %u in bytes", sizeof(long int));
+	printf("Size of float: %u in bytes", sizeof(float));
+
+	return (0);
+
+}
