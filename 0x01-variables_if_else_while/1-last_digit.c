c9f1b5944bdb07bd0e0a183c429b34c5fe2d9541
ufaz <talk2ufaz@gmail.com>
2022-09-09 15:49:20 -0700

    Added:1-Last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,11 +15,11 @@
 int main(void)
 {
 	int n;
+	int last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	
-	int last_digit;
        	last_digit = n%10;
 
 	if (last_digit > 0 && last_digit < 6)
