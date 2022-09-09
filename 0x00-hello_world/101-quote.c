f7dc0d2431cbb488076b7e31cb1e38c20f8ddcf5
ufaz <talk2ufaz@gmail.com>
2022-09-09 01:27:09 -0700

    Added 101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry points
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return 1;
+}
