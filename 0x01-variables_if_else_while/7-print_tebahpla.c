0661e069a491881fde441d5046207216a39f908b
ufaz <talk2ufaz@gmail.com>
2022-09-09 13:51:02 -0700

    Added: 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,29 @@
+/**
+ * File: 7-prints_tehbahpla
+ *
+ * Auth: Umar Faruk A <talk2ufaz@gmail.com>
+ */
+
+#include <stdio.h>
+
+/**
+ *
+ * main - Print alphabets in reverse order
+ *
+ * Return: Always (0)
+ *
+ */
+
+int main(void)
+{
+	int j;
+
+	for (j = 'z'; j >= 'a'; j--)
+	{
+		putchar(j);
+	}
+
+	putchar('\n');
+
+	return 0;
+}
