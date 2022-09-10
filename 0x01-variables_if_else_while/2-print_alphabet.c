ad9eb452cc1fec2b441bf2a77361e2385da42924
ufaz <talk2ufaz@gmail.com>
2022-09-10 05:35:20 -0700

    Added:2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabets in lower case
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		printf("%c", i);
+	}
+	return 0;
+}
