85bdb001704acf4b08e06798b97623a81c9ce4cc
ufaz <talk2ufaz@gmail.com>
2022-09-10 06:36:15 -0700

    Added: 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,26 +1,18 @@
-/**
- * File: 5-print_numbers
- *
- * Auth: Umar Faruk A<talk2ufaz@gmail.com>
- */
-
 #include <stdio.h>
 
 /**
- *
  * main - Prints all single digits numbers
  *
  * Return: 0
- *
  */
-
 int main(void)
 {
-	for (int i = 0; i <= 10; i++)
+	int i;
+
+	for (i = 0; i < 10; i++)
 	{
-		printf("%d",i);
+		printf("%d", i);
 	}
-
+	printf("\n");
 	return (0);
-
 }
