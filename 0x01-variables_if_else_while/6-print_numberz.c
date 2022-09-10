a938ab755d88000ea53e009397d34480f929281b
ufaz <talk2ufaz@gmail.com>
2022-09-10 06:59:59 -0700

    Added:print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit number
+ *	of base 10 starting from 0
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int k;
+	int result;
+
+	for (k = 0; k <= 10; k++)
+	{
+		result = k % 10;
+		putchar(result + '0');
+	}
+	putchar('\n');
+	return (0);
+}
