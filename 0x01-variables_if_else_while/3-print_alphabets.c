90b6f33395c4ab410604d11905850ad0e26df0a8
ufaz <talk2ufaz@gmail.com>
2022-09-10 05:53:17 -0700

    Added: 3-alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabets in l and u
+ * Return: Always (0).
+ */
+int main(void)
+{
+	char s;
+	char u;
+	for (s = 'a'; s <= 'z'; s++)
+	{
+		putchar(s);
+	}
+	for (u = 'A'; u <= 'Z'; u++)
+	{
+		putchar(u);
+	}
+	putchar('\n');
+	return (0);
+}
