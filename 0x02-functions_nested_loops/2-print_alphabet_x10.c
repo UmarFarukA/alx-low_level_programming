4c0a4ec0ef5f4da0669b596129a4f13823256c55
ufaz <talk2ufaz@gmail.com>
2022-09-13 07:42:14 -0700

    Added:2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,4 @@
-#include <stdio.h>
-#include "_putchar.c"
-
-void print_alphabet_x10(void);
-
-/**
- * main - Called the print_alphabet_x10
- *
- * Return: Always (0).
- */
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
+#include "main.h"
 
 /**
  * print_alphabet_x10 - print alphabet 10x.
