c5a73f150bed878d561e43fd8cf40179ef18671a
ufaz <talk2ufaz@gmail.com>
2022-09-13 08:38:18 -0700

    added: 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,16 +8,17 @@
 void print_alphabet_x10(void)
 {
 	char j = 'a';
-	int i = 1;
 
 	while (i <= 10)
 	{
+		char j = 'a';
+
 		while (j <= 'z')
 		{
 			_putchar(j);
 			j++;
 		}
-		_putchar('\n');
 		i++;
+		_putchar('\n');
 	}
 }
