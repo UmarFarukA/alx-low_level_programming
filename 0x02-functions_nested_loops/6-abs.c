9786ce217d560f492fbd321a1579aa92eecd4ff9
ufaz <talk2ufaz@gmail.com>
2022-09-13 09:26:10 -0700

    Added 6-abs.c and main update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <main.h>
+#include <stdlib.h>
+
+/**
+ * _abs - Computes absolute value
+ * @n: The value passed to abs
+ * Return: 1
+ */
+int _abs(int n)
+{
+	_putchar(abs(n));
+	return (1);
+}
