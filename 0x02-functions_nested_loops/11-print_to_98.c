379a110cb542d66207d83f105b28a3612fcc9ee1
ufaz <talk2ufaz@gmail.com>
2022-09-14 02:56:19 -0700

    Added:11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,7 +19,6 @@ void print_to_98(int n)
 			printf("%d, ", k);
 		}
 		printf("%d", last);
-		printf("\n");
 	}
 	else if (n < 98)
 	{
@@ -30,11 +29,9 @@ void print_to_98(int n)
 			printf("%d, ", j);
 		}
 		printf("%d", last);
-		printf("\n");
 	}
 	else if (n == 98)
 	{
 		printf("%d", n);
 	}
-	printf("\n");
 }
