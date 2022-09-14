bb561dca22f38994181df640c3ae63ca127bd50a
ufaz <talk2ufaz@gmail.com>
2022-09-14 04:08:30 -0700

    Added:9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - Prints times table
+ *
+ * Return: Always 0
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			k = i * j;
+			if (k < 10)
+			{
+				printf("%d,  ", k);
+			}
+			else
+			{
+				printf("%d, ", k);
+			}
+		}
+		printf("\n");
+	}
