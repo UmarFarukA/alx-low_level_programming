7aa378d6cabdeeb01442ca16c3740b18928566dc
ufaz <talk2ufaz@gmail.com>
2022-09-15 04:11:11 -0700

    Added:3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - Print numbers 0-9
+ *
+ * Return: void.
+ */
+void print_numbers(void)
+{
+	int j;
+
+	for (j = 0; j <= 9; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+}
