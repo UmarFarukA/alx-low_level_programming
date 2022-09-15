00f974ee0bb8eb908b62fe0a89811448503dde9c
ufaz <talk2ufaz@gmail.com>
2022-09-15 03:52:48 -0700

    Added:0-isupper and main.h

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isupper - Checks if character is upper
+ * @c: THe character passed
+ * Return: 1 if true otherwise 0.
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
