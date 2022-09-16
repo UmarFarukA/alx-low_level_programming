62bc509e8e2237fb1c63653d9f65cfc390e558b1
ufaz <talk2ufaz@gmail.com>
2022-09-16 03:56:52 -0700

    Added:5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,12 +7,12 @@
  */
 void more_numbers(void)
 {
-	char j;
-	char k;
+	int j;
+	int k;
 
-	for (j = '1'; j <= '10'; j++)
+	for (j = 1; j <= 10; j++)
 	{
-		for (k = '0'; k <= '14'; k++)
+		for (k = 0; k <= 14; k++)
 		{
 			_putchar(k);
 		}
