16099659f158e27b4001c2c2581209bf1c0e9e60
ufaz <talk2ufaz@gmail.com>
2022-09-19 14:28:21 -0700

    Added:9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+char *_strcpy(char *dest, char *src);
+
+/**
+ * _strcpy - Copy string from src to dest
+ * @dest: Destination point
+ * #src: Source Point
+ * Return: String of characters
+ */
+char *_strcpy(char *dest, char *src)
+{
+	dest = strcpy(src);
+	return (des);
+}
