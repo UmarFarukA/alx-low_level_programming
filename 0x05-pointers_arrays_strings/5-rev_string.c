fa8c817b9f26cf8e03b5d447be7b34bbe28138a1
ufaz <talk2ufaz@gmail.com>
2022-09-19 08:26:08 -0700

    Added:5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include <strlen>
+#include "main.h"
+
+/**
+ * rev_string - REverse a string
+ * @s: The string to reverse
+ * Return: Nothing
+ */
+void rev_string(char *s)
+{
+	int len;
+	int j;
+
+	len = strlen(s);
+	char rev[len];
+	for (j = len; j >= 0; j--)
+	{
+		if (s[j] != '\0')
+		{
+			rev[len - j] = s[j];
+		}
+		else
+		{
+			continue;
+		}
+	}
+	return (rev);
+}
