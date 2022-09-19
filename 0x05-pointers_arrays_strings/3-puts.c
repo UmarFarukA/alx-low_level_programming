a44c744dcb15b5f2dabc1dcdadec3028483874d8
ufaz <talk2ufaz@gmail.com>
2022-09-19 06:55:02 -0700

    Added:3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - Display a string
+ * @str: The string passed to be printed
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
