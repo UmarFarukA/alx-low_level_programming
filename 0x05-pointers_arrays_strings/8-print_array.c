685bee60d0a43999f6208bfa30e0a5a23427abee
ufaz <talk2ufaz@gmail.com>
2022-09-19 14:15:30 -0700

    Added: 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,11 +12,18 @@ void print_array(int *a, int n)
 {
 	int j;
 
-	for (j = 0; j < n; j++)
+	if (n <= 0)
 	{
-		if (j == n - 1)
-			printf("%d\n", *(a + j));
-		else
-			printf("%d, ", *(a + j));
+		printf("\n\n");
+	}
+	else
+	{
+		for (j = 0; j < n; j++)
+		{
+			if (j == n - 1)
+				printf("%d\n", *(a + j));
+			else
+				printf("%d, ", *(a + j));
+		}
 	}
 }
