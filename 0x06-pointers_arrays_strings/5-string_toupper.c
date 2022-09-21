e317ab6f5da2dc7592f7f8f7bbcff8c39e7572f9
ufaz <talk2ufaz@gmail.com>
2022-09-21 09:16:30 -0700

    ADded: 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include <ctype.h>
+#include <string.h>
+#include "main.h"
+
+char *string_toupper(char *);
+
+/**
+ * string_toupper - Changeletter case t
+ * Return: String
+ */
+char string_toupper(char *)
+{
+	char a[];
+	int j;
+	char k;
+
+	for (j = 0; j < strlen(a); j++)
+	{
+		_putchar(toupper(a[j]);
+	}
+	_putchar('\n');
+}
