5da18000402fe29350dbaa6f5690fc4549c9d0ac
ufaz <talk2ufaz@gmail.com>
2022-09-22 01:16:52 -0700

    Added:1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,7 +10,7 @@ char *_strncat(char *dest, char *src, int n);
  * @n: Number of characters to
  * Return: A pointer to string
  */
-char _strncat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int j;
 
