790542ba36c703040b2c8598fa69d5c160ea6106
ufaz <talk2ufaz@gmail.com>
2022-09-22 08:42:42 -0700

    Added:4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -15,11 +15,9 @@ void reverse_array(int *a, int n)
 
 	for (j = n - 1; j >= 0; j--)
 	{
-		if (j != 0)
-		{
-			printf(", ");
-		}
-		printf("%d", a[j]);
+		if (j == 0)
+			printf("%d\n", *(a));
+		else
+			printf("%d, ", *(a + j));
 	}
-	printf("\n");
 }
