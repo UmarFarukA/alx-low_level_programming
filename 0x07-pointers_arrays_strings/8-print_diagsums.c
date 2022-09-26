b02e4bb4ece31d16588f1993029da77907de0859
ufaz <talk2ufaz@gmail.com>
2022-09-26 14:16:14 -0700

    Added:8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,32 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+void print_diagsums(int *a, int size);
+
+/**
+ * print_diagsums - sum elements in diagonal
+ * @a: array a of elements
+ * @size: size of array
+ */
+void print_diagsums(int *a, int size)
+{
+	int j;
+	int k;
+	int sum;
+
+	sum = 0;
+	k = 0;
+	j = 0;
+	while (k < size)
+	{
+		while (j < size)
+		{
+			sum = sum + *(a[k][j]);
+			break;
+		}
+		k = k + 1;
+		j = j + 1;
+	}
+	return (sum);
+}
