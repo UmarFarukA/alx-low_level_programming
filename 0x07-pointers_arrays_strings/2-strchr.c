c0f35056b0a91349ac18401e2b3b43060ec7a454
ufaz <talk2ufaz@gmail.com>
2022-09-26 07:30:16 -0700

    Added:2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+
+char *_strchr(char *s, char c);
+
+/**
+ * _strchr - Locates a c in s
+ * @s: The string we will c in.
+ * @c: the c we are locating
+ * Return: Match poiter or null
+ */
+char _strchar(char *s, char c)
+{
+	return (strchar(s, c));
+}
