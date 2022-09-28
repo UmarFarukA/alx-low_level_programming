16019f0cbd2bfb276a86acce9ebd136e8872aa63
ufaz <talk2ufaz@gmail.com>
2022-09-28 02:57:38 -0700

    Added:0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+void _puts_recursion(char *s);
+
+/**
+ * _puts_recursion - Prints a string
+ * @s: The string passed
+ * Return: String
+ */
+void _puts_recursion(char *s)
+{
+	return (puts(s));
+}
