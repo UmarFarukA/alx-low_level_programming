70fe921dbcac2ae100e573e65ac8b2c6c6b6761c
ufaz <talk2ufaz@gmail.com>
2022-09-30 15:05:44 -0700

    Addeed::1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Print number of args
+ * @argc: Counts of args
+ * @argv: A vector
+ * Return: Numberof args
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+}
+
