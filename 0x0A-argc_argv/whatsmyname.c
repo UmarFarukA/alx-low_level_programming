dd66f0456028380e29af50049de7525363364043
ufaz <talk2ufaz@gmail.com>
2022-09-30 14:23:31 -0700

    Initial commit

---
+++ 0x0A-argc_argv/whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of program
+ * @argc: Count the number of agrs
+ * @argv: Array of args
+ * Return: 0
+ */
+int main(int argc, char* argv[])
+{
+	printf("%s", arg[0]);
+	return (0);
+}
