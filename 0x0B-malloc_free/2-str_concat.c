364d92eb77bc630ae2ca4a2ac4bee4c110ead94f
ufaz <talk2ufaz@gmail.com>
2022-10-03 10:03:22 -0700

    Added:2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,13 +19,26 @@ char *str_concat(char *s1, char *s2)
 
 	j = 0;
 	if (s1 == NULL)
+	{
 		ptr = malloc(sizeof(s2));
+		ptr = s2;
+		return (ptr);
+	}
 	else if (s2 == NULL)
+	{
 		ptr = malloc(sizeof(s1));
+		ptr = s1;
+		return (ptr);
+	}
 	else if (s2 == NULL && s1 == NULL)
+	{
 		ptr = malloc(2);
+		return (0);
+	}
 	else
+	{
 		ptr = malloc(sizeof(s1) + sizeof(s2));
+	}
 	if (ptr == NULL)
 	{
 		return (NULL);
