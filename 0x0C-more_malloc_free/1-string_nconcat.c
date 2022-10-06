89a63922b8b9d8d0e4cbdb8e2f4983b95d15690f
ufaz <talk2ufaz@gmail.com>
2022-10-06 09:40:29 -0700

    Added:1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,7 +13,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int *ptr;
+	char *ptr;
 	int len;
 
 	len = strlen(s2);
