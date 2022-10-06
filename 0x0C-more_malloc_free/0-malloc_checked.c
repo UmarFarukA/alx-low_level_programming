d892db52b9670724b8565f37e4c48d66be89c319
ufaz <talk2ufaz@gmail.com>
2022-10-06 08:26:38 -0700

    Added:0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+void *malloc_checked(unsigned int b);
+
+/**
+ * malloc_checked - Check for creation of pointer
+ * @b: Exit value
+ * Return: A pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(sizeof(int*) * b);
+	if (ptr != NULL)
+		return (ptr);
+	else
+		exit(0);
+}
