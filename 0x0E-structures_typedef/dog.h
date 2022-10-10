966aab1d57ac4ae403cc773f1443b5b17fe30387
ufaz <talk2ufaz@gmail.com>
2022-10-10 06:52:18 -0700

    Added:dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - Dog structures
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
+ *
+ * Description: This structure dog is all about dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
