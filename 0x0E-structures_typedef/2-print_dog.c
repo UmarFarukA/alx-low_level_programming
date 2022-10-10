edc18f25e4053f7b0f0772f934e1d2458a9a794f
ufaz <talk2ufaz@gmail.com>
2022-10-10 08:38:42 -0700

    Added:2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "dog.h"
+
+void print_dog(struct dog *d);
+
+/**
+ * print_dog - Print dog
+ * @d: Structure d
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		;
+	else if (d->name == NULL)
+	{
+		printf("Name: %s\n", "(nil)");
+	}
+	else if (d->age == NULL)
+	{
+		printf("Age: %.1f\n", "(nil)");
+	}
+	else if (d->owner == NULL)
+	{
+		printf("Owner: %s\n", "(nil)");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %.1f", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
+}
