1a670431139c734c1b329b9363c9b04f82ef2368
ufaz <talk2ufaz@gmail.com>
2022-10-11 08:59:04 -0700

    Added:dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#include <stdlib.h>
-
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - Dog structures
  * @name: Dog name
@@ -22,3 +22,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+#endif
