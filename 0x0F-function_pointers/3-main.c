06e7ec3dbd958ce751d94a0877ff97520391bddd
ufaz <talk2ufaz@gmail.com>
2022-10-12 15:00:47 -0700

    Added:3-All

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+#include "stdio.h"
+#include <stdlib.h">
+
+/**
+ * main - Executes the operations
+ *
+ * Return: Result
+ */
+int main(int argc, char *argv[])
+{
+	int n1;
+	int n2;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	n1 = atoi(argv[1]);
+	op = argv[2];
+	n2 = atoi(argv[3]);
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*op == '/' && n2 == 0) || (*op == '%' && n2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(n1, n2));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+/**
+ * op_add - Add two numbers
+ * @a: First number
+ * @b: Second number
+ * Return: Sum of two numbers
+ */
+int op_add(int a, int b)
+{
+	retunr (a + b);
+}
+
+/**
+ * op_sub - Subtract two integers
+ * @a: First integer
+ * @b: Second Integer
+ * Return: Diff of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul _ Multiply two integers
+ * @a: First integer
+ * @b: Second Integer
+ * Return: Product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divide two integers
+ * @a: First number
+ * @b: Second NUmber
+ * Return: Result of a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Find the modulus of two integers
+ * @a: First number
+ * @b: Second Number
+ * Return: Remain of a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
