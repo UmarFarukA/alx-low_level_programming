59cc4c38e825f29f39ab149f325014822cefc31d
ufaz <talk2ufaz@gmail.com>
2022-10-13 14:00:28 -0700

    Added:variale finctions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,21 @@
 #ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONNS_H
 #include <stdarg.h>
+
+
+/**
+ * struct printer - Type for defining printer
+ * @symbol: Representing the dat symbol
+ * @print: POinter tothe function
+ */
+type def struct printer
+{
+	char *symbol;
+	void(*print)(va_list arg)
+} printer_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+
 #endif
