3f353158e1bf12c02885f119de56ebabeebf5750
ufaz <talk2ufaz@gmail.com>
2022-10-13 03:39:36 -0700

    Added:1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - Print Numbers
+ * @separator: A character that separates numbers
+ * @n: Number of element
+ */
+void print_numbers(const char *separator, unsigned int n, ...)
+{
+	int j;
+
+	if (separator == NULL)
+	{
+	}
+	else
+	{
+		va_list ptr;
+
+		va_start(ptr, n);
+		for (j = 0; j < n; j++)
+		{
+			if (j != n-1)
+				printf("%d%s", va_arg(ptr, int), separator);
+			else
+				printf("%d", va_arg(ptr, int));
+		}
+		va_end(ptr);
+		printf("\n");
+	}
+}
