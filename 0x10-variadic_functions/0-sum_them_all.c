08e103c3194bb316b5c0f3dc0e384df73e653a1c
ufaz <talk2ufaz@gmail.com>
2022-10-13 02:57:59 -0700

    Added:0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+
+/**
+ * sum_them_all - Sum all integers
+ * @n: The number of elements
+ * Return: Sum of elements
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int j;
+	int total;
+
+	total = 0;
+	if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		va_list ptr;
+
+		va_start(ptr, n);
+		for (j = 0; j < n; j++)
+		{
+			total += va_arg(ptr, int);
+		}
+		va_end(ptr);
+		return (total);
+	}
+}
