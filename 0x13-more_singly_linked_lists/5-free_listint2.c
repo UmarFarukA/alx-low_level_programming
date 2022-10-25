5a6e9d550fb7c5428f4d1615a3ff066d4bcfb28a
ufaz <talk2ufaz@gmail.com>
2022-10-25 14:27:51 -0700

    Added:5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,7 +10,9 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (head != NULL)
+	if(*head == NULL)
+		return NULL;
+	while (*head != NULL)
 	{
 		temp = (*head)->next;
 		free(*head);
