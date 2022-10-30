76101e523ecf431a70b07f4af3883ab5c77fe217
ufaz <talk2ufaz@gmail.com>
2022-10-30 09:11:31 -0700

    Added:2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_bit - Returns the of a given value at index
+ * @n: THe number passed
+ * @index: THe index
+ * Return: The value
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsinged long int) * 8))
+	{
+		return (-1);
+	}
+	if ((n & (1 << index)) == 0)
+	{
+		return (0);
+	}
+	return (0);
+}
