a6c3cc034ad08528443559c257d60759bc6142bf
ufaz <talk2ufaz@gmail.com>
2022-10-30 09:32:34 -0700

    Added:3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -6,12 +6,12 @@
  * @index: The index
  * Return: 1 if worked otherwise -1
  */
-int set_bit(unsigned long int n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int) * 8))
 	{
 		return (-1);
 	}
-	*n ^= (1 <> index);
+	*n ^= (1 << index);
 	return (1);
 }
