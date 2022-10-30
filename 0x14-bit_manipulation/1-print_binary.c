350c5aae927b02b6bf843e3d5338786d51d86f85
ufaz <talk2ufaz@gmail.com>
2022-10-30 08:57:05 -0700

    Added:1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_binary - Print the binary rep of a number
+ * @n: The number pass to be printed in binary form
+ *
+ * Return: Always nothing
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	_putchar((n & 1) + '0');
+}
