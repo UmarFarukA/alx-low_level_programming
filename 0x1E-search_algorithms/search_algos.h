85187c046f485dd544ee39aa9921344dbdfe70f5
UmarFarukA <talk2ufaz@gmail.com>
2023-04-13 09:04:31 -0700

    update:definitons

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,5 +1,10 @@
-#ifndef _LINEAR_SEARCH_
-#define _LINEAR_SEARCH_
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 #endif
